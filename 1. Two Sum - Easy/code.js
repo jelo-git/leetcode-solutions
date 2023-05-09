627b0e65ec736e27ec18d1d0e7d33bc2b3946917
jelo-git <batogfilip@gmail.com>
2023-05-09 17:21:12 +0200

    first commit

--- 1. Two Sum - Easy/code.js
+++ 1. Two Sum - Easy/code.js
@@ -4,15 +4,6 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
-    for (n = 0; n < nums.length - 1; n++) {
-        for (m = n + 1; m < nums.length; m++) {
-            if (nums[n] + nums[m] == target) {
-                return [n, m]
-            }
-        }
-    }
-};
-var twoSumSmol = function (nums, target) {
     let dict = {}
     for (n = 0; n < nums.length; n++) {
         if (dict[target - nums[n]] != null)
@@ -20,5 +11,3 @@ var twoSumSmol = function (nums, target) {
         dict[nums[n]] = n
     }
 };
-console.log(twoSum([3, 3], 6))
-console.log(twoSumSmol([3, 3], 6))
